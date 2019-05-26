5429ac1bc781c2673e501facec1d8069aee56fa4
unknown <rmtestard@gmail.com>
2019-05-26 23:14:04 +0200

    loader

---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div class="loader-wrapper">
+      <span class="loader">
+        <span class="loader-inner" />
+      </span>
+    </div>
+  );
+};
+
+export default Loader;
